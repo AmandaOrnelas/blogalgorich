c79aaefca15bb33a1f30187c3680d4257f300325
AmandaOrnelas <amandaoornelas@gmail.com>
2017-12-28 13:53:26 -0200

    Configuring size of text

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+	has_many :posts
+	validates :name, length: { maximum: 50 }
+	
+	validates :name, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+	belongs_to :user
+	belongs_to :category
+	
+	validates :title, length: { maximum: 100 }
+	
+	validates :title, presence: true
+	validates :content, presence: true
+	validates :date, presence: true
+	validates :category_id, presence: true
+	validates :user_id, presence: true
 end
