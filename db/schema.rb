ea6d1b82af1ed4282d626d0faf3549295bebd3ed
AmandaOrnelas <amandaoornelas@gmail.com>
2017-12-30 19:39:28 -0200

    Add some advaced login's functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230171020) do
+ActiveRecord::Schema.define(version: 20171230202803) do
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20171230171020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
